4b42bbedcd26a0d00ce5a35c0eb88dfdd930c695
Nils Asmussen <nils@os.inf.tu-dresden.de>
2021-08-08 13:51:55 +0200

    riscv: improved syscall tracing.

--- arch/riscv/kernel/sysc_trace.c
+++ arch/riscv/kernel/sysc_trace.c
@@ -1,13 +1,18 @@
 #include <linux/linkage.h>
 #include <linux/ptrace.h>
+#include <linux/slab.h>
 
-#define MAX_SYSCALL_TIMES   8192
+#define MAX_SYSCALL_TIMES   65536
+
+struct TracedSyscall {
+    unsigned int no;
+    unsigned long enter_time;
+    unsigned long leave_time;
+};
 
 int sysc_pid = 0;
 unsigned long sysc_counter = 0;
-unsigned int syscall_nos[MAX_SYSCALL_TIMES];
-unsigned long enter_times[MAX_SYSCALL_TIMES];
-unsigned long leave_times[MAX_SYSCALL_TIMES];
+struct TracedSyscall *syscalls = 0;
 
 #undef __SYSCALL
 #define __SYSCALL(nr, call) [nr] = #call,
@@ -28,19 +33,25 @@ static inline unsigned long _get_cycles(void) {
 
 asmlinkage void sysc_trace_enter(unsigned int a0) {
     if(current->pid == sysc_pid) {
-        syscall_nos[sysc_counter] = a0;
-        enter_times[sysc_counter] = _get_cycles();
+        syscalls[sysc_counter].no = a0;
+        syscalls[sysc_counter].enter_time = _get_cycles();
     }
 }
 
 asmlinkage void sysc_trace_exit(void) {
     if(current->pid == sysc_pid) {
-        leave_times[sysc_counter] = _get_cycles();
+        syscalls[sysc_counter].leave_time = _get_cycles();
         sysc_counter = (sysc_counter + 1) % MAX_SYSCALL_TIMES;
     }
 }
 
 asmlinkage long sys_syscreset(int pid) {
+    if(syscalls == 0) {
+        syscalls = kmalloc_array(MAX_SYSCALL_TIMES, sizeof(struct TracedSyscall), GFP_NOWAIT);
+        if(syscalls == 0)
+            panic("unable to allocate syscall tracing array");
+    }
+
     sysc_pid = pid;
     sysc_counter = MAX_SYSCALL_TIMES - 1;
     return 0;
@@ -50,7 +61,8 @@ asmlinkage long sys_sysctrace(void) {
     unsigned long i;
     for(i = 0; i < sysc_counter; ++i) {
         pr_notice(" [%3lu] %s (%d) %011lu %011lu\n",
-            i, sysc_names[syscall_nos[i]], syscall_nos[i], enter_times[i], leave_times[i]);
+            i, sysc_names[syscalls[i].no], syscalls[i].no,
+            syscalls[i].enter_time, syscalls[i].leave_time);
     }
     sysc_pid = 0;
     return 0;
