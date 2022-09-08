8e7693b951b334d4140052fe34f38add75057c3e
Nils Asmussen <nils@os.inf.tu-dresden.de>
2022-09-08 08:49:34 +0200

    Added mwait syscall for DTU-sleep/mwait comparison.

---
+++ arch/x86/power/mwait.c
@@ -0,0 +1,31 @@
+#include <linux/syscalls.h>
+#include <asm/mwait.h>
+
+unsigned long long gem5_debug(unsigned long long msg) {
+    unsigned long long res = 0;
+    __asm__ volatile(
+        ".byte 0x0F, 0x04;"
+        ".word 0x63;"
+        : "=a"(res)
+        : "D"(msg)
+    );
+    return res;
+}
+
+SYSCALL_DEFINE2(mwait, void *, counter, int, user) {
+    if(!user) {
+        gem5_debug(0xDEAD);
+
+        // pr_info("counter=%px, user=%d\n", counter, user);
+
+        // this is not a correct usage of monitor&mwait, but we don't care about the event here, but
+        // only want to measure the latency to put the core to sleep and wakeup again.
+        __monitor(counter, 0, 0);
+        __mwait(0, 0);
+
+        gem5_debug(0xBEEF);
+    }
+    else
+        gem5_debug(0xAFFE);
+    return 0;
+}
