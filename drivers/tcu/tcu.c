85b2b33534b868cd016e05274af0d461fe09c28b
Oliver Portee <oliver.portee@gmail.com>
2022-11-11 13:46:50 +0100

    drivers/tcu.c: add ioctl call to insert entry into tcu tlb
    from now on, the client can ony mmap the unprivileged tcu mmio range

--- drivers/tcu/tcu.c
+++ drivers/tcu/tcu.c
@@ -7,6 +7,80 @@
 #include <linux/device.h>
 #include <linux/kdev_t.h>
 #include <linux/cdev.h>
+#include <linux/ioctl.h>
+#include <linux/io.h>
+
+#define MMIO_PRIV_ADDR 0xf0002000
+#define MMIO_PRIV_SIZE (2 * PAGE_SIZE)
+
+// start address of the private tcu mmio region (is mapped to MMIO_PRIV_ADDR)
+static uint64_t* priv_base = NULL;
+
+typedef struct {
+    uint64_t virt;
+    uint64_t phys;
+    uint32_t perm;
+    uint16_t asid;
+} xlate_fault_arg_t;
+
+#define IOCTL_XLATE_FAULT _IOW('q', 1, xlate_fault_arg_t*)
+
+#define PRIV_CMD     0x1
+#define PRIV_CMD_ARG 0x2
+
+#define IDLE         0x0
+#define INS_TLB      0x3
+
+static void write_priv_reg(unsigned int index, uint64_t val) {
+    iowrite64(val, priv_base + index);
+}
+
+static uint64_t read_priv_reg(unsigned int index) {
+    return ioread64(priv_base + index);
+}
+
+static bool check_priv_error(void) {
+    uint64_t cmd;
+    while (true) {
+        cmd = read_priv_reg(PRIV_CMD);
+        if ((cmd & 0xf) == IDLE) {
+            if (((cmd >> 4) & 0xf) != 0) {
+                return true; // error
+            }
+            return false; // success
+        }
+    }
+}
+
+static void insert_tlb(xlate_fault_arg_t arg) {
+    uint64_t cmd;
+    write_priv_reg(PRIV_CMD_ARG, arg.phys);
+    // TODO: fence
+    cmd = ((uint64_t)arg.asid) << 41
+        | ((arg.virt & PAGE_MASK) << 9)
+        | (arg.perm << 9)
+        | INS_TLB;
+    write_priv_reg(PRIV_CMD, cmd);
+    if (check_priv_error()) {
+        pr_err("failed to insert tlb entry\n");
+    }
+}
+
+static long int tcu_ioctl(struct file *f, unsigned int cmd, unsigned long arg) {
+    xlate_fault_arg_t d;
+    switch (cmd) {
+        case IOCTL_XLATE_FAULT:
+			if (copy_from_user(&d, (xlate_fault_arg_t*)arg, sizeof(xlate_fault_arg_t))) {
+				return -EACCES;
+			}
+            pr_info("received ioctl xlate fault call; virt: %#llx, phys: %#llx, perm: %#x, asid: %#hx\n", d.virt, d.phys, d.perm, d.asid);
+            insert_tlb(d);
+            break;
+        default:
+			return -EINVAL;
+    }
+    return 0;
+}
 
 #ifdef __HAVE_PHYS_MEM_ACCESS_PROT
 #error "__HAVE_PHYS_MEM_ACCESS_PROT is defined"
@@ -40,13 +114,13 @@ static const struct vm_operations_struct mmap_mem_ops = {
 };
 
 #define MMIO_ADDR 0xf0000000
-#define MMIO_SIZE (4 * PAGE_SIZE)
+#define MMIO_SIZE (2 * PAGE_SIZE)
 
 static int tcu_dev_mmap(struct file *file, struct vm_area_struct *vma) {
 	size_t size = vma->vm_end - vma->vm_start;
     // physical address of the mmio area
 	phys_addr_t offset = (phys_addr_t)vma->vm_pgoff << PAGE_SHIFT;
-    pr_info("tcu mmap - start: 0x%lx, end: 0x%lx, size: %lu, offset: 0x%llx\n", vma->vm_start, vma->vm_end, size, offset);
+    pr_info("tcu mmap - start: %#lx, end: %#lx, size: %#lx, offset: %#llx\n", vma->vm_start, vma->vm_end, size, offset);
 
 	/* Does it even fit in phys_addr_t? */
 	if (offset >> PAGE_SHIFT != vma->vm_pgoff) {
@@ -58,7 +132,6 @@ static int tcu_dev_mmap(struct file *file, struct vm_area_struct *vma) {
 		return -EINVAL;
     }
 
-    // TODO: later, size should only be 2 * PAGE_SIZE
     /* We only want to support mapping the tcu mmio area */
     if (offset != MMIO_ADDR || size != MMIO_SIZE) {
         pr_err("tcu mmap invalid area\n");
@@ -87,7 +160,7 @@ static int msg_dev_mmap(struct file *file, struct vm_area_struct *vma) {
 	size_t size = vma->vm_end - vma->vm_start;
     // physical address of the mmio area
 	phys_addr_t offset = (phys_addr_t)vma->vm_pgoff << PAGE_SHIFT;
-    pr_info("tcu msg mmap - start: 0x%lx, end: 0x%lx, size: %lu, offset: 0x%llx\n", vma->vm_start, vma->vm_end, size, offset);
+    pr_info("tcu msg mmap - start: %#lx, end: %#lx, size: %#lx, offset: %#llx\n", vma->vm_start, vma->vm_end, size, offset);
 
 	/* Does it even fit in phys_addr_t? */
 	if (offset >> PAGE_SHIFT != vma->vm_pgoff) {
@@ -137,6 +210,7 @@ static struct class *dev_class;
 static struct file_operations tcu_dev_fops = {
     .owner = THIS_MODULE,
     .mmap = tcu_dev_mmap,
+    .unlocked_ioctl = tcu_ioctl,
 };
 
 static struct file_operations msg_dev_fops = {
@@ -148,7 +222,6 @@ static int open(struct inode *inode, struct file *filp) {
     int minor;
 
     minor = iminor(inode);
-    pr_info("tcu device with minor %d was opened\n", minor);
     if (minor == TCU_MINOR) {
         filp->f_op = &tcu_dev_fops;
     } else if (minor == MSG_MINOR) {
@@ -202,7 +275,18 @@ static int __init tcu_init(void) {
         unregister_chrdev_region(dev, DEV_COUNT);
         class_destroy(dev_class);
     }
-    pr_info("created tcu and tcumsg devices\n");
+    priv_base = (uint64_t*)ioremap(MMIO_PRIV_ADDR, MMIO_PRIV_SIZE);
+    if (!priv_base) {
+        dev_t tcu_dev = MKDEV(major, TCU_MINOR);
+        dev_t msg_dev = MKDEV(major, MSG_MINOR);
+        pr_err("failed to ioremap the private tcu mmio interface\n");
+        device_destroy(dev_class, tcu_dev);
+        device_destroy(dev_class, msg_dev);
+        unregister_chrdev_region(dev, DEV_COUNT);
+        class_destroy(dev_class);
+        return -1;
+    }
+    pr_info("ioctl xlate fault magic number: %#lx\n", IOCTL_XLATE_FAULT);
 
     return 0;
 }
@@ -210,6 +294,7 @@ static int __init tcu_init(void) {
 static void __exit tcu_exit(void) {
     dev_t tcu_dev = MKDEV(major, TCU_MINOR);
     dev_t msg_dev = MKDEV(major, MSG_MINOR);
+    iounmap((void*)priv_base);
     device_destroy(dev_class, tcu_dev);
     device_destroy(dev_class, msg_dev);
     class_destroy(dev_class);
