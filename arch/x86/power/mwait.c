23b9427d61f23f064df0a07172b4eafdc785102f
Nils Asmussen <nils@os.inf.tu-dresden.de>
2024-02-12 17:22:11 +0100

    mwait: use inline assembly.
    The provided function also calls some side-channel mitigations, which we
    don't want to measure in this case.

--- arch/x86/power/mwait.c
+++ arch/x86/power/mwait.c
@@ -21,7 +21,8 @@ SYSCALL_DEFINE2(mwait, void *, counter, int, user) {
         // this is not a correct usage of monitor&mwait, but we don't care about the event here, but
         // only want to measure the latency to put the core to sleep and wakeup again.
         __monitor(counter, 0, 0);
-        __mwait(0, 0);
+	    asm volatile(".byte 0x0f, 0x01, 0xc9;"
+		     :: "a" (0), "c" (0));
 
         gem5_debug(0xBEEF);
     }
